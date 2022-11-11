8335c270e5f6b91ea5e4d98dec2eadb270413d5d
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-11-11 15:57:40 +0530

    Added: program to find the sum using command line argument

---
+++ src/com/bridgelabz/day1/CmdInt.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.day1;
+import java.util.Scanner;
+public class CmdInt {
+
+	public static void main(String[] args) {
+		int x, y,sum;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the first number:");
+		x=sc.nextInt();
+		System.out.println("Enter the second number:");
+		y=sc.nextInt();
+		sum = sum(x,y);
+		System.out.println("The sum of two numbers x and y is:"+sum);
+	}
+	public static int sum(int a,int b) {
+		int sum=a+b;
+		return sum;
+	}
+	}
+
