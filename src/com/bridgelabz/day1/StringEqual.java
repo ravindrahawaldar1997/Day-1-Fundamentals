93bba3646740ee68e8c389ee849ddc8216daee62
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-11-11 15:46:38 +0530

    Added: program Strings are equal or not

---
+++ src/com/bridgelabz/day1/StringEqual.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day1;
+
+public class StringEqual {
+
+	public static void main(String[] args) {
+		String str1="HELLO";
+		String str2="HELLO";
+		System.out.println(str1.equals(str2));
+
+	}
+
+}
