c04ed3a6e6c14bbcbe5843e6a60c9162d27a895f
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-11-11 14:38:51 +0530

    Added: command name file

---
+++ src/day1practiceproblems/CmdName.java
@@ -0,0 +1,14 @@
+package day1practiceproblems;
+
+import java.util.Scanner;
+
+public class CmdName {
+
+	public static void main(String args[]) {
+		Scanner ref = new Scanner (System.in);
+		System.out.println("Write any name here: ");
+		String s=ref.nextLine();
+		System.out.println("Your given name is : "+s);
+	}
+
+}
